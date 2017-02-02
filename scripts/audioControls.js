3b924a40dbfaf0a2bbd6c260fd1b908b1455d096
JeremyADreher <jeremyadreher@gmail.com>
2017-02-01 16:29:19 -0800

    link Index.html to audioControls.js

--- scripts/audioControls.js
+++ scripts/audioControls.js
@@ -1,5 +1,29 @@
 var bgMusic = document.getElementById("bgMusic");
-bgMusic.volume = 1.0;
+bgMusic.volume = 0.0;
 
 var thunder = document.getElementById("thunder");
 thunder.volume = 0.0;
+
+var rain = document.getElementById("rain");
+rain.volume = 0.0;
+
+var laugh = document.getElementById("laugh");
+laugh.volume = 0.0;
+
+var ghost = document.getElementById("ghost1");
+ghost.volume = 0.0;
+
+var swamp = document.getElementbyId("swamp2");
+swamp.volume = 0.0;
+
+var howl = document.getElementById("howl");
+howl.volume = 0.0;
+
+var chains = document.getElementById("m_chains");
+chains.volume = 0.0;
+
+var witches_house = document.getElementbyId("witches_house");
+witches_house.volume = 0.0;
+
+var scream = document.getElementById("scream");
+scream.volume = 0.0;
