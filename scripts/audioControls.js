a636a8ffbc133f49c7e3b1f7cfee860ceab14e8d
JeremyADreher <jeremyadreher@gmail.com>
2017-02-01 18:47:31 -0800

    Add more sounds to audioControls

--- scripts/audioControls.js
+++ scripts/audioControls.js
@@ -27,3 +27,48 @@ witches_house.volume = 0.0;
 
 var scream = document.getElementById("scream");
 scream.volume = 0.0;
+
+var axe = document.getElementById("axe");
+axe.volume = 0.0;
+
+var chomp = document.getElementById("chomp");
+chomp.volume = 0.0;
+
+var coffin_bomb = document.getElementbyId("coffin_bomb");
+coffin_bomb.volume = 0.0;
+
+var coffin_creak = document.getElementbyId("coffin_creak");
+coffin_creak.volume = 0.0;
+
+var door_stone = document.getElementById("door_stone");
+door_stone.volume = 0.0;
+
+var female_scream = document.getElementbyId("female_scream");
+female_scream.volume = 0.0;
+
+var fire = document.getElementById("fire");
+fire.volume = 0.0;
+
+var fisticuffs = document.getElementbyId("fisticuffs");
+fisticuffs.volume = 0.0;
+
+var footsteps_stone = document.getElementbyId("footsteps_stone");
+footsteps_stone.volume = 0.0;
+
+var footsteps_metal = document.getElementbyId("footsteps_metal");
+footsteps_metal.volume = 0.0;
+
+var footsteps_outside = document.getElementbyId("footsteps_outside");
+footsteps_outside.volume = 0.0;
+
+var footsteps_wood = document.getElementbyId("footsteps_wood");
+footsteps_wood.volume = 0.0;
+
+var growl = document.getElementbyId("growl");
+growl.volume = 0.0;
+
+var male_scream = document.getElementbyId("male_scream");
+male_scream.volume = 0.0;
+
+
+
