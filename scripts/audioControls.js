9fee49f2c46664398d62006440f78e13236b7b06
JeremyADreher <jeremyadreher@gmail.com>
2017-02-03 09:56:54 -0800

    Adjust volume from 0.0 to 1.0

--- scripts/audioControls.js
+++ scripts/audioControls.js
@@ -1,74 +1,74 @@
 var bgMusic = document.getElementById("bgMusic");
-bgMusic.volume = 0.0;
+bgMusic.volume = 1.0;
 
 var thunder = document.getElementById("thunder");
-thunder.volume = 0.0;
+thunder.volume = 1.0;
 
 var rain = document.getElementById("rain");
-rain.volume = 0.0;
+rain.volume = 1.0;
 
 var laugh = document.getElementById("laugh");
-laugh.volume = 0.0;
+laugh.volume = 1.0;
 
 var ghost = document.getElementById("ghost1");
-ghost.volume = 0.0;
+ghost.volume = 1.0;
 
 var swamp = document.getElementbyId("swamp2");
-swamp.volume = 0.0;
+swamp.volume = 1.0;
 
 var howl = document.getElementById("howl");
-howl.volume = 0.0;
+howl.volume = 1.0;
 
 var chains = document.getElementById("m_chains");
-chains.volume = 0.0;
+chains.volume = 1.0;
 
 var witches_house = document.getElementbyId("witches_house");
-witches_house.volume = 0.0;
+witches_house.volume = 1.0;
 
 var scream = document.getElementById("scream");
-scream.volume = 0.0;
+scream.volume = 1.0;
 
 var axe = document.getElementById("axe");
-axe.volume = 0.0;
+axe.volume = 1.0;
 
 var chomp = document.getElementById("chomp");
-chomp.volume = 0.0;
+chomp.volume = 1.0;
 
 var coffin_bomb = document.getElementbyId("coffin_bomb");
-coffin_bomb.volume = 0.0;
+coffin_bomb.volume = 1.0;
 
 var coffin_creak = document.getElementbyId("coffin_creak");
-coffin_creak.volume = 0.0;
+coffin_creak.volume = 1.0;
 
 var door_stone = document.getElementById("door_stone");
-door_stone.volume = 0.0;
+door_stone.volume = 1.0;
 
 var female_scream = document.getElementbyId("female_scream");
-female_scream.volume = 0.0;
+female_scream.volume = 1.0;
 
 var fire = document.getElementById("fire");
-fire.volume = 0.0;
+fire.volume = 1.0;
 
 var fisticuffs = document.getElementbyId("fisticuffs");
-fisticuffs.volume = 0.0;
+fisticuffs.volume = 1.0;
 
 var footsteps_stone = document.getElementbyId("footsteps_stone");
-footsteps_stone.volume = 0.0;
+footsteps_stone.volume = 1.0;
 
 var footsteps_metal = document.getElementbyId("footsteps_metal");
-footsteps_metal.volume = 0.0;
+footsteps_metal.volume = 1.0;
 
 var footsteps_outside = document.getElementbyId("footsteps_outside");
-footsteps_outside.volume = 0.0;
+footsteps_outside.volume = 1.0;
 
 var footsteps_wood = document.getElementbyId("footsteps_wood");
-footsteps_wood.volume = 0.0;
+footsteps_wood.volume = 1.0;
 
 var growl = document.getElementbyId("growl");
-growl.volume = 0.0;
+growl.volume = 1.0;
 
 var male_scream = document.getElementbyId("male_scream");
-male_scream.volume = 0.0;
+male_scream.volume = 1.0;
 
 
 
