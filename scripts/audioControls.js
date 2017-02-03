5a3f670626cf63c18faf87784cce96bfccd8b213
JeremyADreher <jeremyadreher@gmail.com>
2017-02-03 11:15:12 -0800

    Add more sounds to audioControls

--- scripts/audioControls.js
+++ scripts/audioControls.js
@@ -70,5 +70,167 @@ growl.volume = 1.0;
 var male_scream = document.getElementbyId("male_scream");
 male_scream.volume = 1.0;
 
+var page_turn = document.getElementById("page_turn");
+page_turn.volume = 1.0;
+
+var pistol = document.getElementbyId("pistol");
+pistol.volume = 1.0;
+
+var pump_shotgun = document.getElementById("pump_shotgun");
+pump_shotgun.volume = 1.0;
+
+var rifle_shot = document.getElementById("rifle_shot");
+rifle_shot.volume = 1.0;
+
+var rustle = document.getElementById("rustle");
+rustle.volume = 1.0;
+
+var shotgun_shot = document.getElementById("shotgun_shot");
+shotgun_shot.volume = 1.0;
+
+var sobbing = document.getElementById("sobbing");
+sobbing.volume = 1.0;
+
+var door_wood = document.getElementById("door_wood");
+door_wood.volume = 1.0;
+
+var stab = document.getElementById("stab");
+stab.volume = 1.0;
+
+var wind = document.getElementById("wind");
+wind.volume = 1.0;
+
+var wind_outside = document.getElementById("wind_outside");
+wind_outside.volume = 1.0;
+
+var wind_open = document.getElementById("wind_open");
+wind_open.volume = 1.0;
+
+var spike = document.getElementById("spike");
+spike.volume = 1.0;
+
+var footsteps_frantic = document.getElementById("footsteps_frantic");
+footsteps_frantic.volume = 1.0;
+
+var door_metal = document.getElementById("door_metal");
+door_metal.volume = 1.0;
+
+var floor_creak = document.getElementById("floor_creak");
+floor_creak.volume = 1.0;
+
+var decapitation = document.getElementById("decapitation");
+decapitation.volume = 1.0;
+
+var tearing = document.getElementById("tearing");
+tearing.volume = 1.0;
+
+var whoosh = document.getElementById("whoosh");
+whoosh.volume = 1.0;
+
+var man_growl = document.getElementById("man_growl");
+man_growl.volume = 1.0;
+
+var fast_heartbeat = document.getElementById("fast_heartbeat");
+fast_heartbeat.volume = 1.0;
+
+var slow_heartbeat = document.getElementById("slow_heartbeat");
+slow_heartbeat.volume = 1.0;
+
+var long_heartbeat = document.getElementById("long_heartbeat");
+long_heartbeat.volume = 1.0;
+
+var crowd_laughing = document.getElementById("crowd_laughing");
+crowd_laughing.volume = 1.0;
+
+var rage_of_blades = document.getElementById("rage_of_blades");
+rage_of_blades.volume = 1.0;
+
+var locked_door = document.getElementById("locked_door");
+locked_door.volume = 1.0;
+
+var pin_drop = document.getElementById("pin_drop");
+pin_drop.volume = 1.0;
+
+var walking_away = document.getElementById("walking_away");
+walking_away.volume = 1.0;
+
+var tense = document.getElementById("tense");
+tense.volume = 1.0;
+
+var tense2 = document.getElementById("tense2");
+tense2.volume = 1.0;
+
+var tense3 = document.getElementById("tense3");
+tense3.volume = 1.0;
+
+var panic = document.getElementById("panic");
+panic.volume = 1.0;
+
+var demon_girl = document.getElementById("demon_girl");
+demon_girl.volume = 1.0;
+
+var stair_climb = document.getElementById("stair_climb");
+stair_climb.volume = 1.0;
+
+var flesh_split = document.getElementById("flesh_split");
+flesh_split.volume = 1.0;
+
+var blood_drip = document.getElementById("blood_drip");
+blood_drip.volume = 1.0;
+
+var kick_door_down = document.getElementById("kick_door_down");
+kick_door_down.volume = 1.0;
+
+var gun_fight = document.getElementById("gun_fight");
+gun_fight.volume = 1.0;
+
+var men_screaming = document.getElementById("men_screaming");
+men_screaming.volume = 1.0;
+
+var threeshots = document.getElementById("threeshots");
+threeshots.volume = 1.0;
+
+var thump = document.getElementById("thump");
+thump.volume = 1.0;
+
+var yelp = document.getElementById("yelp");
+yelp.volume = 1.0;
+
+var begging = document.getElementById("begging");
+begging.volume = 1.0;
+
+var guncock = document.getElementById("guncock");
+guncock.volume = 1.0;
+
+var earthquake = document.getElementById("earthquake");
+earthquake.volume = 1.0;
+
+var metal_latch = document.getElementById("metal_latch");
+metal_latch.volume = 1.0;
+
+var move_coffin = document.getElementById("move_coffin");
+move_coffin.volume = 1.0;
+
+var punch = document.getElementById("punch");
+punch.volume = 1.0;
+
+var click = document.getElementById("click");
+click.volume = volume;
+
+var bulletwhiz = document.getElementById("bulletwhiz");
+bulletwhiz.volume = 1.0;
+
+var dog_eating = document.getElementById("dog_eating");
+dog_eating.volume = 1.0;
+
+var man_chuckle = document.getElementById("man_chuckle");
+man_chuckle.volume = 1.0;
+
+
+
+
+
+
+
 
 
